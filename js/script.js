14322eb8f8dbdfd28f00b8b8cfad207a086f1bbd
Adadri <86448925+Adadri@users.noreply.github.com>
2021-07-18 20:43:11 -0300

    desafio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    let data = {
+        nome,
+        email,
+        telefone,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+    
+})
